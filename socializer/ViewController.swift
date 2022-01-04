7e0fd9ea1a598f9e59ec46784ec07e093c624b0c
Talha Rasool <talharasool700@gmail.com>
2022-01-04 15:44:38 +0500

    Initial Commit

---
+++ socializer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  socializer
+//
+//  Created by mac on 04/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
